6634c7a1d9232e228574b3fe380715a819df3933
Lakshya Lakhotia <lakshyalakhotia2@gmail.com>
2026-08-06 19:44:53 +0530

    Rewrote some UI components for better readability.
    Made UI look better, modern and like modern GNOME application.

--- TestingAdw.py
+++ TestingAdw.py
@@ -9,7 +9,7 @@ Adw.init()
 
 class MyApp(Adw.Application):
   def __init__(self):
-    super().__init__(application_id = 'com.natic.UiTesting')
+    super().__init__(application_id = 'com.Realchill.Natica')
   
   def do_activate(self):
     window = Adw.ApplicationWindow(application = self)
@@ -17,50 +17,62 @@ class MyApp(Adw.Application):
     window.set_default_size(500,500)
     
     header=Adw.HeaderBar()
-    title = Gtk.Label(label='UI Testing')
+    title = Gtk.Label(label='Natica')
     header.set_title_widget(title)
     
     main_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
 
     self.stack = Gtk.Stack()
     
-    sidebar = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing = 6)
-    sidebar.set_size_request(180,-1)
+    sidebar = Gtk.ListBox()
+    sidebar.set_selection_mode(Gtk.SelectionMode.SINGLE)
+    sidebar.add_css_class('navigation-sidebar')
+    sidebar.set_size_request(220,-1)
     sidebar.set_margin_top(10)
     sidebar.set_margin_bottom(10)
     sidebar.set_margin_start(10)
     sidebar.set_margin_end(10)
     
-    home = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing=10)
-    home.set_halign(Gtk.Align.CENTER)
-    home.set_valign(Gtk.Align.CENTER)
-    home.append(Gtk.Label(label='Home'))
-       
-    projects = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing=10)
-    projects.set_valign(Gtk.Align.CENTER)
-    projects.set_halign(Gtk.Align.CENTER)
-    projects.append(Gtk.Label(label='Projects'))
+
     
-    self.stack.add_named(home,'home')
-    self.stack.add_named(projects,'projects')
+    self.stack.add_named(self.create_page("Home"), "home")
+    self.stack.add_named(self.create_page("Projects"), "projects")
+    self.stack.add_named(self.create_page("Reports"), "reports")
+    self.stack.add_named(self.create_page("Statistics"), "stats")
+    self.stack.add_named(self.create_page("Settings"), "settings")
     
-    homeButton = Gtk.Button(label='Home')
-    homeButton.connect("clicked", lambda b:self.stack.set_visible_child_name('projects'))
-    projects.append(homeButton)
+    self.pages = [
+      ("🏠 Home",'home'),
+      ("📁 Projects",'projects'),
+      ('📊 Reports','reports'),
+      ('📈 Statistics','stats'),
+      ("⚙ Settings", "settings"),
+    ]
     
-    projectsButton = Gtk.Button(label='Projects')
-    projectsButton.connect("clicked",lambda b: self.stack.set_visible_child_name('home'))
-    home.append(projectsButton)
+    for title, page_name in self.pages:
+      self.stack.add_named(self.create_page(title),page_name)
     
-    reportsButton = Gtk.Button(label='Reports')
-    # reportsButton.connect('clicked',self.)
-  
+    for title, page_name in self.pages:
+      row = Gtk.ListBoxRow()
+      
+      label = Gtk.Label(label=title)
+      label.set_halign(Gtk.Align.START)
+      
+      row.set_child(label)
+      
+      sidebar.append(row)
+    
+    sidebar.select_row(sidebar.get_row_at_index(0))  
+      
+    sidebar.connect("row-selected",self.change_page)
     
     
-    
-    
-    main_box.append(self.stack)
     main_box.append(sidebar)
+    main_box.append(self.stack)
+    
+    self.stack.set_hexpand(True)
+    self.stack.set_vexpand(True)
+    
     toolbar = Adw.ToolbarView()
     toolbar.add_top_bar(header)
     toolbar.set_content(main_box)
@@ -68,11 +80,24 @@ class MyApp(Adw.Application):
     window.set_content(toolbar)
     window.present()
     
-  def goHome(self, button):
-    self.stack.set_visible_child_name('home')
+  def create_page(self, name):
+    page = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing = 10)
+                
+    page.set_margin_top(20)
+    page.set_margin_bottom(20)
+    page.set_margin_start(20)
+    page.set_margin_end(20)
+                
+    label = Gtk.Label(label=name)
+    page.append(label)
+    return page
     
-  def goProjects(self, button):
-    self.stack.set_visible_child_name('projects')
+  def change_page(self, listbox, row):
+    if row is None:
+      return
+    self.stack.set_visible_child_name(self.pages[row.get_index()][1])
+    
+                
 
 test = MyApp()
 test.run()
